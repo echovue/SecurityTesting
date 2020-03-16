7e07257658e0bb974fbc922ed374993fa5a1d6c1
Mike Mackrory <mike@echovue.com>
2020-03-16 09:52:11 -0700

    CLean up

--- PythonScripts/packetSniffing.py
+++ PythonScripts/packetSniffing.py
@@ -4,10 +4,10 @@ import struct
 
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
-    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+    return format_mac_addr(dest_mac), format_mac_addr(src_mac), socket.htons(proto), data[14:]
 
 
-def get_mac_addr(bytes_addr):
+def format_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
 
@@ -18,10 +18,10 @@ def main():
     while True:
         raw_data, addr = conn.recvfrom(65535)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-       # if eth_proto == 8:
-        print('\nEthernet Frame:')
-        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
-        print(data)
+        if eth_proto == 8:
+            print('\nEthernet Frame:')
+            print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+            print(data)
 
 
 if __name__ == "__main__":
