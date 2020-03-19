8b6ec433a23abea574db0738c06e79203790ed30
Mike Mackrory <mike@echovue.com>
2020-03-18 18:25:50 -0700

    Clean up

--- PythonScripts/packetInjection.py
+++ PythonScripts/packetInjection.py
@@ -3,25 +3,25 @@ import struct
 from random import randint
 
 
-def createIcmpPacket():
+def create_icmp_packet():
     type = 8
     code = 0
     chksum = 0
     id = randint(0, 0xFFFF)
     seq = 1
-    real_chksum = checksum(struct.pack("!BBHHH", type, code, chksum, id, seq))
-    packet = struct.pack("!BBHHH", type, code, socket.htons(real_chksum), id, seq)
+    checksum = calculate_checksum(struct.pack("!BBHHH", type, code, chksum, id, seq))
+    packet = struct.pack("!BBHHH", type, code, socket.htons(checksum), id, seq)
     print(packet)
     return packet
 
 
-def checksum(data):
+def calculate_checksum(packet):
     s = 0
-    n = len(data) % 2
-    for i in range(0, len(data) - n, 2):
-        s += data[i] + (data[i + 1] << 8)
+    n = len(packet) % 2
+    for i in range(0, len(packet) - n, 2):
+        s += packet[i] + (packet[i + 1] << 8)
     if n:
-        s += data[i + 1]
+        s += packet[i + 1]
     while s >> 16:
         s = (s & 0xFFFF) + (s >> 16)
     s = ~s & 0xFFFF
@@ -30,7 +30,7 @@ def checksum(data):
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    s.sendto(createIcmpPacket(), ("172.16.0.111", 80))
+    s.sendto(create_icmp_packet(), ("172.16.0.111", 80))
 
 
 if __name__ == "__main__":
